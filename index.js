8820cf10e20f8ee878a68dc1b7ed82d8654f462e
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-19 20:47:26 +0200

    wip

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const PORT = 5000;
 //middleware
 app.use(bodyParser.json());
 
+//routes
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+
 //application listen for incoming request
 app.listen(PORT, ()=> console.log(`Server running on port: http://localhost:${POST}`));
+// app.listen(5000, function(){
+//     console.log("info",'Server is running at port : ' + 5000);
+// });
