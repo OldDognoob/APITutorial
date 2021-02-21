0fbf472b61186813c5f58675a869ff9c26090ba3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-21 11:59:42 +0200

    wip

--- routes/users.js
+++ routes/users.js
@@ -31,7 +31,26 @@ router.post('/', (req,res) => {
 //users/2 => req.params(parameters to our function)
 router.get('/:id', (req, res) => {
     const {id} = req.params;
-    res.send(req.params);
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+});
+// we create a callback function with request , response
+router.delete('/:id', (req,res)=>{
+  //get the id from request params
+  const{id}= req.params;
+  //we want to remove the element from the array we use the filter method
+  // we got the user for each iteration
+  // whatever is equal to true we keep the array
+  //whatever is equal to false we remove it
+  // we need to keep all the users except the one who is in line 40
+
+// how it works: 
+/* we dont want to keep john with id 123 in the array. 
+The use of the unequality operator says since john id is equal with the id 123
+this, will return false and so John will be removed. On the other hand
+Jane id 321 is not  indeed equal means it is true and we keep her id.  */
+  users = users.filter((user)=>user.id != id);
+  res.send(`User with the id ${id} deleted from the database.`);
 });
 
 export default router;
